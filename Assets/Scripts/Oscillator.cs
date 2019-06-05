c6563f74a8d2259709c70d00f12eb3cd2ffe1d2f
João Traini <joao.traini@gmail.com>
2019-06-05 18:04:31 -0300

    3_PB_61

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour
 {
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
 
-    //todo remove from inspector later
-    [Range (0,1)] [SerializeField] float movementFactor; //0 for not moved, 1 for fully moved
-
-    Vector3 startingPos; // must be stored for absolute movement
+    float movementFactor; //0 for not moved, 1 for fully moved
+    Vector3 startingPos;
 
     void Start()
     {
@@ -19,6 +18,12 @@ public class Oscillator : MonoBehaviour
 
     void Update()
     {
+        float cycles = Time.time / period; // grows continually from 0
+
+        const float tau = Mathf.PI * 2; // about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau); // goes from -1 to +1
+
+        movementFactor = rawSinWave / 2f + 0.5f;
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
